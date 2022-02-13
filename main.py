7698c0428ba857fa4ae4b8065ad0ebfcda45814c
Mihail Karaev <karaevmihail@gmail.com>
2022-02-13 15:43:02 +0500

    changes

--- main.py
+++ main.py
@@ -2,91 +2,10 @@ import torch
 from torch import load, nn
 import cv2
 import sys
-import matplotlib.pyplot as plt
+from generator import GeneratorUNET
 
-PATH_TO_GEN = 'generator_v2_20epochs_loss_13.097846984863281'
-IMAGE_SIZE = 256
-
-from torch import nn
-import torch
-class GeneratorUNET(nn.Module):
-
-    def __init__(self, input_nc=3, output_nc=3, nf=64, use_dropout=False):
-        super(GeneratorUNET, self).__init__()
-        self.input_nc = input_nc
-        self.output_nc = output_nc
-        self.nf = nf
-        self.dropout = use_dropout
-
-        self.down_conv1 = nn.Conv2d(input_nc, nf, 4, stride=2, padding=1, bias=False)
-        self.down_conv2 = nn.Conv2d(nf, nf * 2, 4, stride=2, padding=1, bias=False)
-        self.down_conv3 = nn.Conv2d(nf * 2, nf * 4, 4, stride=2, padding=1, bias=False)
-        self.down_conv4 = nn.Conv2d(nf * 4, nf * 8, 4, stride=2, padding=1, bias=False)
-        self.down_conv5 = nn.Conv2d(nf * 8, nf * 8, 4, stride=2, padding=1, bias=False)
-        self.down_conv6 = nn.Conv2d(nf * 8, nf * 8, 4, stride=2, padding=1, bias=False)
-        self.down_conv7 = nn.Conv2d(nf * 8, nf * 8, 4, stride=2, padding=1, bias=False)
-        self.down_bottle_conv = nn.Conv2d(nf * 8, nf * 8, 4, stride=2, padding=1, bias=False)
-
-        self.up_bottle_conv = nn.ConvTranspose2d(nf * 8, nf * 8, 4, stride=2, padding=1, bias=False)
-        self.up_conv7 = nn.ConvTranspose2d(nf * 16, nf * 8, 4, stride=2, padding=1, bias=False)
-        self.up_conv6 = nn.ConvTranspose2d(nf * 16, nf * 8, 4, stride=2, padding=1, bias=False)
-        self.up_conv5 = nn.ConvTranspose2d(nf * 16, nf * 8, 4, stride=2, padding=1, bias=False)
-        self.up_conv4 = nn.ConvTranspose2d(nf * 16, nf * 4, 4, stride=2, padding=1, bias=False)
-        self.up_conv3 = nn.ConvTranspose2d(nf * 8, nf * 2, 4, stride=2, padding=1, bias=False)
-        self.up_conv2 = nn.ConvTranspose2d(nf * 4, nf, 4, stride=2, padding=1, bias=False)
-        self.up_conv1 = nn.ConvTranspose2d(nf * 2, output_nc, 4, stride=2, padding=1, bias=True)
-
-        self.up_relu = nn.ReLU(True)
-        self.down_relu = nn.LeakyReLU(0.2, True)
-        self.tanh = nn.Tanh()
-
-        self.bn = nn.BatchNorm2d(nf)
-        self.bn2 = nn.BatchNorm2d(nf * 2)
-        self.bn4 = nn.BatchNorm2d(nf * 4)
-        self.bn8 = nn.BatchNorm2d(nf * 8)
-
-    def forward(self, x):
-        x0 = self.down_relu(self.down_conv1(x))
-        x1 = self.down_relu(self.bn2(self.down_conv2(x0)))
-        x2 = self.down_relu(self.bn4(self.down_conv3(x1)))
-        x3 = self.down_relu(self.bn8(self.down_conv4(x2)))
-        x4 = self.down_relu(self.bn8(self.down_conv5(x3)))
-        x5 = self.down_relu(self.bn8(self.down_conv6(x4)))
-        x6 = self.down_relu(self.bn8(self.down_conv7(x5)))
-        xu = self.up_relu(self.down_bottle_conv(x6))
-
-        xu = self.bn8(self.up_bottle_conv(xu))
-        xu = torch.cat([xu, x6], 1)
-        xu = self.up_relu(xu)
-
-        xu = self.bn8(self.up_conv7(xu))
-        xu = torch.cat([xu, x5], 1)
-        xu = self.up_relu(xu)
-
-        xu = self.bn8(self.up_conv6(xu))
-        xu = torch.cat([xu, x4], 1)
-        xu = self.up_relu(xu)
-
-        xu = self.bn8(self.up_conv5(xu))
-        xu = torch.cat([xu, x3], 1)
-        xu = self.up_relu(xu)
-
-        xu = self.bn4(self.up_conv4(xu))
-        xu = torch.cat([xu, x2], 1)
-        xu = self.up_relu(xu)
-
-        xu = self.bn2(self.up_conv3(xu))
-        xu = torch.cat([xu, x1], 1)
-        xu = self.up_relu(xu)
-
-        xu = self.bn(self.up_conv2(xu))
-        xu = torch.cat([xu, x0], 1)
-        xu = self.up_relu(xu)
-
-        xu = self.tanh(self.up_conv1(xu))
-
-        return xu
-
+PATH_TO_GEN = 'NEW512generator_v2_30 epochs loss 8.945698738098145'
+IMAGE_SIZE = 512
 
 if __name__ == "__main__":
     device='cpu'
@@ -100,11 +19,10 @@ if __name__ == "__main__":
         raise BaseException
 
     input_path = sys.argv[1]
-
     dir_list = input_path.split(sep='\\')
     filename = dir_list[-1]
     image = cv2.imread(input_path)
-    image = image.resize(image, (IMAGE_SIZE,IMAGE_SIZE))
+    image = cv2.resize(image, (IMAGE_SIZE,IMAGE_SIZE))
     image = torch.Tensor(image) / 255
     image = image.to(device)
     image = torch.permute(image, (2, 1, 0)).unsqueeze(0)
--- utils/make_edges.py
+++ utils/make_edges.py
@@ -5,7 +5,7 @@ from os.path import isfile, join
 import numpy as np
 
 
-ORIGINAL_PATH = 'F:\edges2cars\cars_generator\cars_train\cars_train\\'
+ORIGINAL_PATH = 'F:\edges2cars\cars_generator\cars512nobg\\'
 GEN_PATH = 'F:\edges2cars\cars_generator\\'
 RESIZE = 512
 LOWER_THRESH = 255/3
@@ -17,25 +17,26 @@ def get_contour(img_gray):
     thresh = cv2.Canny(img_gray, LOWER_THRESH, HIGHER_THRESH)
     contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE , method=cv2.CHAIN_APPROX_NONE)
     blank_image = np.ones((RESIZE,RESIZE,3), np.uint8) * 255
-    cv2.drawContours(image=blank_image, contours=contours, contourIdx=-1, color=(0, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+    cv2.drawContours(image=blank_image, contours=contours, contourIdx=-1, color=(0, 0, 0), thickness=1, lineType=cv2.LINE_AA)
     return blank_image
 
-
+input_images_path = f'output{RESIZE}nobg'
+output_images_path = f'input{RESIZE}nobg'
 
 
 if __name__ == "__main__":
     onlyfiles = [f for f in listdir(ORIGINAL_PATH) if isfile(join(ORIGINAL_PATH, f))]
     os.chdir(GEN_PATH)
     try:
-        os.makedirs(f'output{RESIZE}')
-        os.makedirs(f'input{RESIZE}')
+        os.makedirs(input_images_path)
+        os.makedirs(f'input{RESIZE}nobg')
     except FileExistsError:
         pass
     for file in onlyfiles:
         print(file)
         image = cv2.imread(ORIGINAL_PATH + file, cv2.IMREAD_COLOR)
         image = cv2.resize(image, (RESIZE,RESIZE))
-        print(cv2.imwrite(f'output{RESIZE}' + '\\' + file, image))
+        print(cv2.imwrite(output_images_path + '\\' + file, image))
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         contour = get_contour(gray_image)
-        cv2.imwrite(f'input{RESIZE}' + '\\' + file, contour)
+        cv2.imwrite(input_images_path + '\\' + file, contour)
